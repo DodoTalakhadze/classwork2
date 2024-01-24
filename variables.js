8d83e23b180de0d033dabfa4c95e95f9466de3dc
DodoTalakhadze <dtalakhadze2@gmail.com>
2024-01-24 19:40:16 +0400

    lesson3

---
+++ variables.js
@@ -0,0 +1,54 @@
+// var firsname = "George";
+// let surname = "Qiria";
+// const age = 23;
+
+// let username = promt("Enter your username");
+// console.log("Hello Mr/Ms", username);
+
+// let firstname = "Reziko"; // string
+// let age = 30; // number 
+// let salary = 987.54; // number
+// let isMarried = true; // false // boolean
+
+// let arrived; // undefined
+
+// console.log(arrived);
+
+// let applesInBasket = null;  
+    
+
+// let firstname = promt("Enter your first name");
+// let lastname = promt("Enter your last name");
+// alert(`Great! Nice to meet you Mr. ${firstname} ${lastname}`);
+
+
+// global scope
+// function scope
+// block scope 
+
+
+// ობიექტის მაგალითი 
+// let person = {
+//     name: 'John',
+//     age: 20
+// }; 
+// console.log(person);
+
+
+
+// array example რამდენიმე მონაცემის ტიპი შეიძლება ჩაიწეროს
+// მასივში ათვლა იწყება ნულიდან
+// let children = ["nino", "nika", "lulu", 54, null];
+// console.log(children[0]);
+
+// children.push("cotniko"); //დამატება
+// let child = promt("enter child name")
+// console.log(child);
+
+// children.pop(); //წაშლა
+
+// console.log(children);
+
+// ჩამატებისთვის შეიძლება წერტილის გამოყენება 
+// personalbar.age = 30; //დაამატებს 30ს
+// წაშლისთვის detele person.surname //surnameს წაშლის 
